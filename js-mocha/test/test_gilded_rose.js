b8ea0ad5caaf91e19d28c2b6050214c41137cd6f
unknown <rellings@gmu.edu>
2019-07-14 20:55:33 -0400

    Added tests for JS mocha implementation

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -3,9 +3,96 @@ var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const gildedRose = new Shop([ new Item("fixme", 0, 0) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("fixme");
   });
+
+  it("Sulfuras does not degrade in quality", function(){
+    const gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros',0,50)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+  })
+
+  it("Aged Brie Increases in quality over time", function(){
+    const gildedRose = new Shop([ new Item('Aged Brie',3,10)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11)
+  })
+
+  it("Aged Brie increases in quality at twice the rate after sell by", function(){
+    const gildedRose = new Shop([ new Item('Aged Brie',0,10)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12)
+  })
+
+  it("Items do not exceed 50 in quality", function(){
+    const gildedRose = new Shop(
+      [ 
+        new Item('Aged Brie',1,50),
+        new Item('Backstage passes to a TAFKAL80ETC concert',1,50),
+        new Item('Aged Brie',0,50),
+        new Item('Backstage passes to a TAFKAL80ETC concert',6,50)
+      ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50)
+    expect(items[1].quality).to.equal(50)
+  })
+
+  it("BackStage pass incrases in quality by 2 within ten days of sell by date", function(){
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert',10,10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12)
+  })
+
+  it("BackStage pass incrases in quality by 3 within five days of sell by date", function(){
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert',5,10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(13)
+  })
+
+  it("BackStage pass quality is zero after sell by date", function(){
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert',0,10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0)
+  })
+
+  it("Items degrade by 1 in quality everday before sellBy date", function(){
+    const gildedRose = new Shop([ new Item('foo',10,10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(9)
+  })
+
+  it("Items degrade by 2 in quality everday after sellBy date", function(){
+    const gildedRose = new Shop([ new Item('foo',0,10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(8)
+  })
+
+  it("Items can't have negative quality", function(){
+    const gildedRose = new Shop([ 
+      new Item('foo',10,0),
+      new Item('foo',0,0),
+      new Item('Conjured - Shield',10,1),
+      new Item('Conjured - Shield',0,1)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0)
+    expect(items[1].quality).to.equal(0)
+    expect(items[1].quality).to.equal(0)
+    expect(items[1].quality).to.equal(0)
+  })
+
+  it("Conjured Items degrade at twice the rate", function(){
+    const gildedRose = new Shop([ 
+      new Item('Conjured - Spear',10,10),
+      new Item('Conjured - Axe',0,10),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(8)
+    expect(items[1].quality).to.equal(6)
+  })
 
 });
